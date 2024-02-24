a358dace412745db360b1c0fc847b8f759349257
nlkim <nlkim.hse@gmail.com>
2024-02-24 16:31:52 +0900

    added go to item page test

---
+++ src/main/java/pages/men_bottoms/FirstItemPage.java
@@ -0,0 +1,21 @@
+package pages.men_bottoms;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class FirstItemPage {
+
+    private final SelenideElement itemName = $("span[data-ui-id='page-title-wrapper']");
+    private final ElementsCollection itemPrices = $$("span[data-price-type='finalPrice']");
+
+    @Step("Checking that correct item was added to cart")
+    public void checkItemNameAndPrice(String name, String price) {
+        itemName.shouldHave(text(name));
+        itemPrices.first().shouldHave(text(price));
+    }
+}
---
+++ src/test/java/tests/men_bottoms/GoToItemPageTest.java
@@ -0,0 +1,34 @@
+package tests.men_bottoms;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.home.HomePage;
+import pages.login_user.LoginPage;
+import pages.men_bottoms.*;
+import tests.AbstractTest;
+import utils.User;
+
+public class GoToItemPageTest extends AbstractTest {
+    @DisplayName("Check the functionality of placing an order")
+    @Test
+    void goToItemPageTest() {
+        LoginPage loginPage = new HomePage()
+                .openSignInPage();
+
+        HomePage homePage = loginPage
+                .loggingIn(User.getEmail(), User.getPassword());
+
+        MenBottomsPage menBottomsPage = homePage
+                .clickMenBottoms();
+
+        String firstItemPrice = menBottomsPage
+                .getFirstItemPrice();
+        String firstItemName = menBottomsPage
+                .getFirstItemName();
+
+        FirstItemPage firstItemPage = menBottomsPage
+                .clickOnFirstItem();
+
+        firstItemPage.checkItemNameAndPrice(firstItemName, firstItemPrice);
+    }
+}
